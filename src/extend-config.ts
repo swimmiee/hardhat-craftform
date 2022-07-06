e8acef2d7a83b15b1a4274660c24b8a780c0f0a0
이수영 <dannylisa67@gmail.com>
2022-07-06 18:23:16 +0900

    undo seperate extendConfig

--- src/extend-config.ts
+++ src/extend-config.ts
@@ -1,35 +1,35 @@
-import { extendConfig, extendEnvironment, task } from "hardhat/config";
-import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
-import { Craftform } from "./craftform/class";
-// import GenerateCrafts from "./utils/generate-crafts";
-import { normalizePath } from "./utils/normalize-path";
-import "./type-extensions";
+// import { extendConfig, extendEnvironment, task } from "hardhat/config";
+// import { lazyObject } from "hardhat/plugins";
+// import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
+// import { Craftform } from "./craftform/class";
+// // import GenerateCrafts from "./utils/generate-crafts";
+// import { normalizePath } from "./utils/normalize-path";
+// import "./type-extensions";
 
 
-extendConfig(
-  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
-    config.paths.crafts = normalizePath(
-      config,
-      userConfig.paths?.crafts,
-      "crafts"
-    );
-  }
-);
+// extendConfig(
+//   (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
+//     config.paths.crafts = normalizePath(
+//       config,
+//       userConfig.paths?.crafts,
+//       "crafts"
+//     );
+//   }
+// );
 
-extendEnvironment((hre) => {
-  const { config, hardhatArguments, tasks, run, network, artifacts } = hre;
+// extendEnvironment((hre) => {
+//   const { config, hardhatArguments, tasks, run, network, artifacts } = hre;
 
-  // artifacts.getArtifactPaths().then(console.log);
-  //
-  // => Result: 
-  // [
-  //   '/Users/swimmie/test/hardhat-test/artifacts/contracts/Test1.sol/Test1.json',
-  //   '/Users/swimmie/test/hardhat-test/artifacts/contracts/Test2.sol/Test2.json',
-  //   '/Users/swimmie/test/hardhat-test/artifacts/hardhat/console.sol/console.json'
-  // ]
+//   // artifacts.getArtifactPaths().then(console.log);
+//   //
+//   // => Result: 
+//   // [
+//   //   '/Users/swimmie/test/hardhat-test/artifacts/contracts/Test1.sol/Test1.json',
+//   //   '/Users/swimmie/test/hardhat-test/artifacts/contracts/Test2.sol/Test2.json',
+//   //   '/Users/swimmie/test/hardhat-test/artifacts/hardhat/console.sol/console.json'
+//   // ]
 
-  hre.craftform = lazyObject(() => new Craftform());
-});
+//   hre.craftform = lazyObject(() => new Craftform());
+// });
 
-// task("crafts", "Generate craft.ts file", GenerateCrafts)
+// // task("crafts", "Generate craft.ts file", GenerateCrafts)
