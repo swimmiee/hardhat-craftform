a14da7fe1882b1f8a1019e948163e20f59cb7ef0
이수영 <dannylisa67@gmail.com>
2022-07-08 17:49:20 +0900

    get method return interface

--- src/craftTypeFactory/craftform-definition.ts
+++ src/craftTypeFactory/craftform-definition.ts
@@ -1,14 +1,15 @@
 export const getCraftformDefinitionCode = (contractNames:string[]) => {
     const coreImports = `import { CraftDeployOptions, GetContractProps } from 'hardhat-craftform/dist/core'`
-    const imports = contractNames.map(name => {
-        return `import { ${name}Args } from './${name}.craft'`
-    }).join('\n')
+    const imports = contractNames.map(name => `
+import { ${name} } from '../${name}';
+import { ${name}Args, ${name}Config } from './${name}.craft';
+`.trim()).join('\n')
 
     const getFunctionDeclares = contractNames.map(name => `
         get(
             contractName: '${name}',
             props: GetContractProps
-        ): Promise<void>
+        ): Promise<Craft<${name}, ${name}Config>>
     `).join('\n')
 
     const deployFunctionDeclares = contractNames.map(name => `
--- src/types/core.ts
+++ src/types/core.ts
@@ -1,8 +1,13 @@
 import { FactoryOptions } from "@nomiclabs/hardhat-ethers/types";
 import { ethers } from "ethers";
 import { DeployOptions } from "hardhat-deploy/dist/types";
+import { BaseContract } from "ethers"
+import { BaseConfig } from "../decorators";
 
 export type ClassType<T = any> = new () => T
+export type Craft<Contract extends BaseContract, Config extends BaseConfig> = Contract & {
+  config: Config
+} 
 export interface GetContractPropsBase {
   chain?: string;
   signerOrOptions?: ethers.Signer | FactoryOptions
